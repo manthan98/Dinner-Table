458394592d7760e307e3e3e9bccc3999d4583234
Austin Atmaja <austin.atmaja@gmail.com>
2017-03-11 15:34:59 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 
 function signInGoogle(){
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
+  // ...
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
