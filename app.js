4a7aef4ec94559cb8182e08e6d95e1115011847c
Manthan Shah <manthan711@gmail.com>
2017-03-12 11:31:43 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -33,16 +33,20 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
 }
 
 var EventForm = document.getElementById("EventForm");
+var EventRef = firebase.database().ref();
 
+var notifCounter = 0;
 
 function writeEvent(){
-    var EventRef = firebase.database().ref();
 	EventRef.push().set({
 		Description: EventForm.description.value,
 		FoodType: EventForm.foodType.value,
-		Poster: name
-    Location: EventForm.address.value,
+		Poster: name,
+        Location: EventForm.address.value
 	});
+
+	
+	var pos;
 
   function geocodeAddress(geocoder, resultsMap) {
     var address = Location;
@@ -67,7 +71,75 @@ function writeEvent(){
 	*/
 }
 
+var userv2 = document.getElementById("userbtn2");
+var uname = document.getElementById("username");
+
 document.getElementById("userbtn").innerHTML = name;
-document.getElementById("userbtn2").innerHTML = name;
-document.getElementById("username").innerHTML = name;
+if(userv2.innerHTML != null){
+    userv2.innerHTML = name;
+}
+if (uname.innerHTML != null){
+    uname.innerHTML = name;
+}
 document.getElementById("profilepicture").src = photoUrl;
+
+var poster;
+var loc;
+var infoOne;
+var infoTwo;
+var infoThree;
+
+
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: {lat: 43.472180, lng: -80.544686}
+        });
+        var geocoder = new google.maps.Geocoder();
+        //This is where initMap is called
+        //eventlistener added to the database reference
+        EventRef.on('child_added', function(snapshot) {
+          loc = snapshot.child("Location").val();
+          infoOne = loc;
+          infoTwo = snapshot.child("Description").val();
+          infoThree = snapshot.child("FoodType").val()
+          poster = snapshot.child("Poster").val();
+          if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+            nextAddress--;
+            delay++;
+          }
+          else{
+            geocodeAddress(geocoder, map);
+          }
+        });
+      }
+
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = loc;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+        
+            var infowindow = new google.maps.InfoWindow({
+				  content: poster + " posted a new event!" + '<br />' + 'Location: ' + infoOne + '<br />' + 'Description: ' + infoTwo + '<br />' + 'Food Type: ' + infoThree
+				});
+			makeInfoWindowEvent(map, infowindow, marker);
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
+
+      function makeInfoWindowEvent(map, infowindow, marker) {
+        google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+        });
+      }
+      
+     
+      
+      
